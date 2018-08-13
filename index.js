b2e39782a71db3aac07e4944481c970ed53f141f
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-08-13 14:08:29 -0400

    finished js basics control flow lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your code in this file!
+// function addFive(someNumber) {
+//   let result
+//   if (someNumber > 0) {
+//     result = someNumber + 5;
+//   }
+//   return result
+// }
+
+function scuberGreetingForFeet(someNumber) {
+  let result
+  if (someNumber <= 400) {
+    result = "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else {
+    result = "No can do."
+  }
+  return result
+}
+
+function ternaryCheckCity(someCity) {
+  let result
+  if (someCity === "NYC") {
+    result = "Ok, sounds good."
+  }
+  else {
+    result = "No go."
+  }
+  return result
+}
+
+function switchOnCharmFromTip(someTip) {
+  let result
+  if (someTip === "generous") {
+    result = "Thank you so much."
+  }
+  else if (someTip === "not as generous") {
+    result = "Thank you."
+  }
+  else {
+    result = "Bye."
+  }
+  return result
+}
