dd3afb94a615d83f2626466d7bc7b3558a7772f5
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-13 14:30:47 -0400

    ternary hernary

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  let result
+  if (someNumber < 400) {
+    result = 'This one is on me!';
+  }
+  else if (someNumber > 2000 && someNumber < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (someNumber > 2500) {
+    result = 'No can do.';
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  let result
+  city === 'NYC' ? result = 'Ok, sounds good.' : result = 'No go.';
+  return result;
+}
+
+function switchOnCharmFromTip(tipAmount) {
+  let result
+  switch (tipAmount) {
+    case 'generous':
+      result = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      result = 'Thank you.'
+      break;
+    default:
+      result = 'Bye.'
+      break;
+  }
+  return result
+}
