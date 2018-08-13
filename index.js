060b14887bc49cc2843f295bbf77b0b6d6996eae
Katie Weber <k.weber0893@gmail.com>
2018-08-13 10:54:26 -0400

    Basic control flow

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(numberOfFeet) {
+  let result
+  if (numberOfFeet <= 400) {
+    result = "This one is on me!"
+  }
+  else if (numberOfFeet > 1999 && numberOfFeet < 2499) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else {
+    result = "No can do."
+  }
+  return result
+};
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+  };
+};
